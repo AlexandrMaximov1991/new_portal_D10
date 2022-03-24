2e5443bb32d9070a1d9868d481c89529077d8de7
Name <maximovyaa@gmail.com>
2022-03-24 12:03:06 +0300

    '1'

--- new_portal/news_portal/signals.py
+++ new_portal/news_portal/signals.py
@@ -8,3 +8,11 @@ from .tasks import post_save_post, notify_managers_posts
 def create_post():
     post_save_post.delay()
     notify_managers_posts.delay()
+
+
+
+# # так тоже не работает!
+# @receiver(post_save)
+# def signals(**kwargs):
+#     post_save_post()
+#     notify_managers_posts()
